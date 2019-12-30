f618b9a7cd464ac6a4e33694280fea3ec3edc56c
github username <github email address>
2019-12-30 15:41:32 -0500

    MenuController

--- bin/run.rb
+++ bin/run.rb
@@ -3,3 +3,5 @@ require_relative '../config/environment'
 
 
 puts "HELLO WORLD"
+
+#MenuController.new(user).main_menu
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/trip.db')
 require_all 'app'
+require_all 'controller'
