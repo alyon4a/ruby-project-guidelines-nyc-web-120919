0b6d46aa6cd8945503d047a513fc5de0ba231696
github username <github email address>
2020-01-02 12:25:56 -0500

    wish list completed

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -3,7 +3,17 @@ class Attraction < ActiveRecord::Base
     has_many :users, through: :reviews
 
     has_many :attraction_likes
+
     def user_likes
         self.attraction_likes.map{|likes| likes.user}
     end
+
+    def add_review(review_hash, user)
+        review_hash[:user_id] = user.id
+        review_hash[:attraction_id] = self.id
+        Review.create(review_hash)
+        user.reviews.reload
+        attraction.reviews.reload
+    end
+
 end
